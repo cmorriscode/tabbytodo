0f9228c735de41697dc4ad7fb6fc2c26cecec846
Charles Morris <cmorriscode@gmail.com>
2021-04-29 21:09:59 -0400

    recovery

--- tabbytodo/src/main.js
+++ tabbytodo/src/main.js
@@ -2,10 +2,12 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import store from "./store";
 import MainBtn from "./components/MainBtn.vue";
+import UUID from "vue-uuid";
 
 const app = createApp(App);
 
 app.use(store);
+app.use(UUID);
 
 app.component("main-btn", MainBtn);
 
